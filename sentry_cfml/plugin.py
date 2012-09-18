f6b1a3c98cad3128ca7153ce8384b3791bbf1691
Jay McEntire <jay.mcentire@gmail.com>
2012-09-18 11:33:56 -0600

    adding plugin code from raven-cfml.

---
+++ sentry_cfml/plugin.py
@@ -0,0 +1,15 @@
+import sentry_pluginname
+from sentry.plugins import Plugin
+
+
+class PluginName(Plugin):
+    title = 'Sentry CFML'
+    slug = 'sentry-cfml'
+    description = 'A plugin for CFML that adds a custom interface specific to request variables used in CFML'
+    version = sentry_pluginname.VERSION
+
+    author = 'Jay McEntire'
+    author_url = 'https://github.com/jmacul2/sentry-cfml'
+
+    def widget(self, request, group, **kwargs):
+        pass
